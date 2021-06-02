d465bf08316bf8c422f3f013eb74b6b25f9ddbfa
drstrangegithub <72202965+drstrangegithub@users.noreply.github.com>
2021-06-02 18:23:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let Discord = require("discord.js");
+let client = new Discord.Client();
+
+client.on("ready", () => {
+  client.user.setPresence({ activity: { name: "Listening to nobody"}}) //this is the bot status code..
+})
+
+client.on("guildMemberAdd", member => {
+if (member.guild.id === "Your server id here") { //enable your developer mode in your discord settings and right click your server.. then you should see copy id option...
+  client.channels.cache.get("your welcome channel id here").send(`Welcome ${member}!! Thanks for joining the server :D`)
+}
+})
+
+client.login("your top secret token here..")
+
+//Made by 365 ɢᴀᴍɪɴɢ ɴ ᴍᴏʀᴇ_2.0#6766 DONOT share without credits!!
